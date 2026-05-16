78eaf7aaa157b0c673b7707952bd995bf5b66dc6
zainulabidinmuhammad40-bit <zainulabidinmuhammad40@gmail.com>
2026-05-17 01:43:03 +0500

    p1

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import home from './weather card/home'
+function App() {
+  return (
+    <div>
+      <h1>Weather App Home</h1><h3>City:</h3>
+      <input placeholder='enter your city name'></input> <br></br>
+       <br></br>
+
+      <button>Get Weather</button>
+    </div>
+  )
+}
+
+export default App
